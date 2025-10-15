4146e36066a2013300674610a1acdf0372a15bb6
samkooijman99 <samkooijman@live.nl>
2025-10-15 16:27:05 +0200

    copy target to image_tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -133,7 +133,7 @@ resource "google_cloud_run_v2_job" "dbt_cloudrunjob" {
       containers {
         name = var.gcp_cloud_run_job
         image = data.google_artifact_registry_docker_image.my_image.self_link
-        args    = ["uv run dbt build --target cicd && cp -r target /data/"]
+        args    = ["uv run dbt build --target cicd && cp -r target /data/${var.image_tag}"]
         env {
           name  = "PG_HOSTNAME"
           value = "/cloudsql/${google_sql_database_instance.postgres_instance.connection_name}" #unix socket #google_sql_database_instance.postgres_instance.public_ip_address #tcp connection
